d48508be70f1f58c148466531403a33823b6465d
Zachary Canann <zacharycanann@gmail.com>
2026-07-23 23:54:09 -0700

    retain shared guard storage byte

--- crates/pipeline/mwcc-syntax-trees-to-machine-code/src/lib.rs
+++ crates/pipeline/mwcc-syntax-trees-to-machine-code/src/lib.rs
@@ -623,6 +623,7 @@ pub fn lower_function(
     generator.schedule_shared_right_float_product_pair();
     generator.schedule_shared_float_store_literal(function);
     generator.schedule_frame_vector_accumulation();
+    generator.schedule_shared_guard_storage_byte();
     generator.schedule_guarded_bitfield_storage_cache();
     generator.schedule_inlined_sign_store();
     generator.schedule_inlined_acceleration_select();
